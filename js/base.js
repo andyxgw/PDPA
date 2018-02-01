4a1dee3e5d7550e68bac9946b221cba9db78ef67
Andy Xu <andy@couleurs-productions.com>
2018-02-01 17:33:17 +0800

    Changes

--- js/base.js
+++ js/base.js
@@ -9,7 +9,7 @@ var LastPage = TotalPages - 1;
 var ContentPage = 2;
 var PagesArray = [];
 
-var StartPageNo = 3; 
+var StartPageNo = 3;
 var MenuPageNo = 2;
 var PageNo = 1;
 
@@ -25,7 +25,7 @@ var introScreen = $('#introScreen');
 var mainContent = $('#main_content');
 var content = $('#content');
 
-var containerOriginalWidth = 1400; 
+var containerOriginalWidth = 1400;
 var containerOriginalHeight = 1000; // Footer will take up 140px
 
 PagesArray.length = TotalPages;
@@ -49,27 +49,25 @@ window.addEventListener("resize", resizeWindow);
 
 /* Execution of functions upon document loaded */
 $(document).ready(function(e) {
-	
+
 	resizeWindow();
 	mainContent.show();
-	
+
 	$("#sidebar").mCustomScrollbar({
 		theme: "minimal"
 	});
 
 	$('#dismiss, .overlay').on('click', function () {
 		$('#sidebar').removeClass('active');
-		$('.overlay').fadeOut();
+
 	});
 
-	$('#menu').on('click', function () {
-		console.log("hello");
+	$('#sidebarCollapse').click(function (e) {
 		$('#sidebar').addClass('active');
-		$('.overlay').fadeIn();
 		$('.collapse.in').toggleClass('in');
 		$('a[aria-expanded=true]').attr('aria-expanded', 'false');
 	});
-	
+
 
 });
 
@@ -80,47 +78,47 @@ function resizeWindow() {
     var width, height;
 	var winWidth, winHeight;
     var containerNewWidth, containerNewHeight;
-	
-	if (typeof (window.innerWidth) == 'number') 
+
+	if (typeof (window.innerWidth) == 'number')
 	{
 		winWidth = window.innerWidth;
 		winHeight = window.innerHeight;
-	} 
-	else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) 
+	}
+	else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight))
 	{
 		//IE 6+ in 'standards compliant mode'
 		winWidth = document.documentElement.clientWidth;
 		winHeight = document.documentElement.clientHeight;
-	} 
+	}
 	else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
 		//IE 4 compatible
 		winWidth = document.body.clientWidth;
 		winHeight = document.body.clientHeight - 10;
 	}
-	
-	
-	
+
+
+
 	if(winWidth<=winHeight)
 	{
 		pixelRatio = 10 * (winWidth/containerOriginalWidth) + 'px';
 		if(mainContent.offsetHeight > winHeight && mainContent.offsetWidth != winWidth)
-			pixelRatio = 10 * (winHeight/containerOriginalHeight) + 'px';				
+			pixelRatio = 10 * (winHeight/containerOriginalHeight) + 'px';
 	}
-	if(winWidth>winHeight) 
+	if(winWidth>winHeight)
 	{
 		pixelRatio = 10 * (winHeight/containerOriginalHeight) + 'px';
 		if(mainContent.offsetWidth >= winWidth)
-		{				
+		{
 			if(winWidth>winHeight && mainContent.offsetWidth==winWidth)
 			{
-				if(mainContent.offsetHeight < winHeight)		
+				if(mainContent.offsetHeight < winHeight)
 					pixelRatio = 10 * (winWidth/containerOriginalWidth) + 'px';
 			}
-		}	
+		}
 	}
 	splashScreen.css("font-size", pixelRatio);
-	introScreen.css("font-size", pixelRatio);	
-	mainContent.css("font-size", pixelRatio);	
+	introScreen.css("font-size", pixelRatio);
+	mainContent.css("font-size", pixelRatio);
 }
 
 
@@ -131,7 +129,3 @@ function disableScrollBars() {
 	document.body.style.overflow = "hidden";
 	document.body.scroll = "no"; // ie only
 }
-
-
-
-
